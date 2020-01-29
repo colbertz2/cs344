bed74beef82e382acd20f97a47b9f6b5160b0746
Zach Colbert <colbertz@oregonstate.edu>
2020-01-29 12:56:18 -0800

    Add mypython.py with shebang and comment

---
+++ projects/py/mypython.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+#############################################################################
+# TITLE: mypython.py                                                        #
+# AUTHOR: Zach Colbert                                                      #
+# EMAIL: colbertz@oregonstate.edu                                           #
+# DESCRIPTION:  Sends 3 lines of 10 characters each (plus newline) to       #
+#               stdout, then to 3 files in the same directory as this file. #
+#               Then, sends 2 random ints [1, 42] to stdout, followed by    #
+#               their product.                                              #
+# NOTES: CS 344, W 2020 (online)                                            #
+#############################################################################
+
