e74a815e977e0015fbe9256802669d3b01cb8808
Zach Colbert <colbertz@oregonstate.edu>
2020-02-04 17:58:14 -0800

    bRaCkEtS!

--- projects/2/src/buildrooms.c
+++ projects/2/src/buildrooms.c
@@ -111,37 +111,49 @@ int main() {
  **********************/
 
 // Allocate a room struct and assign it a name
-struct room* room_create();
+struct room* room_create() {
+}
 
 // Deallocate room struct
-void room_free(struct room*);
+void room_free(struct room*) {
+}
 
 // Select a room from array by index
-struct room* room_get(struct room**, int);
+struct room* room_get(struct room**, int) {
+}
 
 // Select a room at random from array
-struct room* room_$get(struct room**);
+struct room* room_$get(struct room**) {
+}
 
 // Return 1 if outbound connections are allowed for a room, 0 otherwise
-int room_allowOutbound(struct room*);
+int room_allowOutbound(struct room*) {
+}
 
 // Return 1 if rooms are already connected, 0 otherwise
-int room_isConnected(struct room*, struct room*);
+int room_isConnected(struct room*, struct room*) {
+}
 
 // Connect two rooms (without validation)
-void room_connect(struct room*, struct room*);
+void room_connect(struct room*, struct room*) {
+}
 
 // Return 1 if rooms are the same (equality comparison)
-int room_cmp(struct room*, struct room*);
+int room_cmp(struct room*, struct room*) {
+}
 
 // Given a file descriptor, write contents of struct to a room file
-void room_export(struct room*, int);
+void room_export(struct room*, int) {
+}
 
 // Return 1 if all rooms have 3-6 outbound connections, 0 otherwise
-int map_isfull(struct room**);
+int map_isfull(struct room**) {
+}
 
 // Loop until map is full, creating random room connections
-void map_$fill(struct room**);
+void map_$fill(struct room**) {
+}
 
 // Assign start, end, mid room types
-void map_$setTypes(struct room**);
+void map_$setTypes(struct room**) {
+}
